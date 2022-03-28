9660b17503b4dffca5b53a91878d4d17580a125c
ChrisSweeneyIRIS <chrissweeney.cs@gmail.com>
2022-03-28 21:33:51 +0100

    Added checkbox for disabling button

--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ import './App.css';
 
 function App() {
   const [ buttonColor, setButtonColor ] = useState('red');
+  const [disabled, setDisabled ] = useState(false);
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
 
   return (
     <div>
       <button 
-        style = {{ backgroundColor: buttonColor}}
-        onClick = { () => setButtonColor(newButtonColor) }>
+        style = {{ backgroundColor: buttonColor, color: 'white' }}
+        onClick = { () => setButtonColor(newButtonColor) }
+        disabled = { disabled }>
         Change to {newButtonColor}
       </button>
+      <input
+        type = "checkbox"
+        id = "enable-button-checkbox"
+        aria-checked={ disabled }
+        defaultChecked = { disabled }
+        onChange = { (e) => setDisabled(e.target.checked) } />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -22,3 +22,26 @@ it('should turn the button blue when clicked', () => {
 
   expect(colorButton.textContent).toBe('Change to red');
 });
+
+it('should have the checkbox unchecked initially', () => {
+  render(<App/>);
+
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  expect(colorButton).toBeEnabled();
+
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+});
+
+it('should disable the button when checked', () => {
+  render(<App/>);
+
+  const button = screen.getByRole('button');
+  const checkbox = screen.getByRole('checkbox');
+
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled();
+});
