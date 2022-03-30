8338793af8ecb1d1c1357d5ccf369c312fc42512
ChrisSweeneyIRIS <chrissweeney.cs@gmail.com>
2022-03-30 11:50:32 +0100

    Added prettier and eslint

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 export function replaceCamelWithSpace(colorName) {
-  return colorName.replace(/\B([A-Z])\B/g, ' $1');
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
 }
 
 function App() {
-  const [ buttonColor, setButtonColor ] = useState('MediumVioletRed');
-  const [disabled, setDisabled ] = useState(false);
-  const newButtonColor = buttonColor === 'MediumVioletRed' ? 'MidnightBlue' : 'MediumVioletRed';
+  const [buttonColor, setButtonColor] = useState("MediumVioletRed");
+  const [disabled, setDisabled] = useState(false);
+  const newButtonColor =
+    buttonColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed";
 
   return (
     <div>
-      <button 
-        style = {{ backgroundColor: disabled ? 'gray' : buttonColor }}
-        onClick = { () => setButtonColor(newButtonColor) }
-        disabled = { disabled }>
-        Change to { replaceCamelWithSpace(newButtonColor) }
+      <button
+        style={{ backgroundColor: disabled ? "gray" : buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
+      >
+        Change to {replaceCamelWithSpace(newButtonColor)}
       </button>
       <input
-        type = "checkbox"
-        id = "disable-button-checkbox"
-        aria-checked={ disabled }
-        defaultChecked = { disabled }
-        onChange = { (e) => setDisabled(e.target.checked) } />
+        type="checkbox"
+        id="disable-button-checkbox"
+        aria-checked={disabled}
+        defaultChecked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
 
-        <label htmlFor = "disable-button-checkbox">Disable button</label> 
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
